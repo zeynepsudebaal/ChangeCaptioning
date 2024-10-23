bc1e97075c5657e1a4b9f59e0f7964f9850a861f
zeynepsudebaal <155081439+zeynepsudebaal@users.noreply.github.com>
2024-10-23 10:03:42 +0200

    Update meteor.py

--- eval_func/meteor/meteor.py
+++ eval_func/meteor/meteor.py
@@ -1,39 +1,18 @@
-#!/usr/bin/env python
-
-# Python wrapper for METEOR implementation, by Xinlei Chen
-# Acknowledge Michael Denkowski for the generous discussion and help
-
-import os
-import sys
 import subprocess
 import threading
-
-# Assumes meteor-1.5.jar is in the same directory as meteor.py.  Change as needed.
-METEOR_JAR = 'meteor-1.5.jar'
-
-
-# print METEOR_JAR
 
 class Meteor:
 
     def __init__(self):
-        self.env = os.environ
-        self.env['LC_ALL'] = 'en_US.UTF_8'
-        self.meteor_cmd = ['java', '-jar', '-Xmx2G', METEOR_JAR, \
-                           '-', '-', '-stdio', '-l', 'en', '-norm']
-        self.meteor_p = subprocess.Popen(self.meteor_cmd, \
-                                         cwd=os.path.dirname(os.path.abspath(__file__)), \
-                                         stdin=subprocess.PIPE, \
-                                         stdout=subprocess.PIPE, \
-                                         stderr=subprocess.PIPE,
-                                         env=self.env, universal_newlines=True, bufsize=1)
-        # Used to guarantee thread safety
+        self._start_meteor()
+
+    def _start_meteor(self):
+        self.meteor_cmd = ['java', '-jar', '-Xmx2G', 'meteor-1.5.jar', '-', '-', '-stdio', '-l', 'en', '-norm']
+        self.meteor_p = subprocess.Popen(self.meteor_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, bufsize=1)
         self.lock = threading.Lock()
 
-    def compute_score(self, gts, res):
-
+    def compute_score(self, gts, res, retries=3):
         scores = []
-
         eval_line = 'EVAL'
         self.lock.acquire()
         for i in range(len(res)):
@@ -41,25 +20,32 @@ class Meteor:
             stat = self._stat(res[i][0], gts[i])
             eval_line += ' ||| {}'.format(stat)
 
-        # Send to METEOR
-        self.meteor_p.stdin.write(eval_line + '\n')
+        try:
+            self.meteor_p.stdin.write(eval_line + '\n')
+        except BrokenPipeError as e:
+            print(f"BrokenPipeError: {e}. Retrying...")
+            self._restart_meteor_process()
+            return self.compute_score(gts, res, retries - 1)  # Retry
 
-        # Collect segment scores
         for i in range(len(res)):
             score = float(self.meteor_p.stdout.readline().strip())
             scores.append(score)
 
-        # Final score
         final_score = float(self.meteor_p.stdout.readline().strip())
         self.lock.release()
 
         return final_score, scores
 
+    def _restart_meteor_process(self):
+        self.meteor_p.stdin.close()
+        self.meteor_p.kill()
+        self.meteor_p.wait()
+        self._start_meteor()
+
     def method(self):
         return "METEOR"
 
     def _stat(self, hypothesis_str, reference_list):
-        # SCORE ||| reference 1 words ||| reference n words ||| hypothesis words
         hypothesis_str = hypothesis_str.replace('|||', '').replace('  ', ' ')
         score_line = ' ||| '.join(('SCORE', ' ||| '.join(reference_list), hypothesis_str))
         self.meteor_p.stdin.write(score_line + '\n')
